5a67257b81685e02f863ade6cad1834f3c77f5ae
MaxGB23 <cb147.gonzalez.ballesteros.max@gmail.com>
2024-11-22 21:52:44 -0600

    Deploy 3

--- resources/js/Pages/Categorias/Create.vue
+++ resources/js/Pages/Categorias/Create.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import NavBack from '@/components/NavBack.vue';
+import NavBack from '@/Components/NavBack.vue';
 import { Head,useForm, Link } from '@inertiajs/vue3';
 import { watch } from 'vue';
 import InputError from '@/Components/InputError.vue';
