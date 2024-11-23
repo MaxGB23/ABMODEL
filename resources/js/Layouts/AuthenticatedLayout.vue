613c75a92aa8ef63caf3aead0d1c8f1110e590b6
MaxGB23 <cb147.gonzalez.ballesteros.max@gmail.com>
2024-11-22 22:00:01 -0600

    Deploy 3

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -6,7 +6,7 @@ import DropdownLink from '@/Components/DropdownLink.vue';
 import NavLink from '@/Components/NavLink.vue';
 import ResponsiveNavLink from '@/Components/ResponsiveNavLink.vue';
 import { Link } from '@inertiajs/vue3';
-import NavBack from '@/components/NavBack.vue';
+import NavBack from '@/Components/NavBack.vue';
 
 </script>
 
